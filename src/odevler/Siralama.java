f9eb15556f01b29e310d69d51d2e03ee24ae3086
Murat Batak <muratbatak052005@gmail.com>
2021-06-19 12:21:45 +0300

    Sayıları Büyükten Küçüğe Sıralayan Program

---
+++ src/odevler/Siralama.java
@@ -0,0 +1,92 @@
+package odevler;
+
+import java.util.Scanner;
+
+public class Siralama {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int a,b,c;
+        System.out.print("Lütfen sıralanacak ilk sayıyı giriniz: ");
+        a = input.nextInt();
+        System.out.print("Lütfen sıralanacak ikinci sayıyı giriniz: ");
+        b = input.nextInt();
+        System.out.print("Lütfen sıralanacak üçüncü sayıyı giriniz: ");
+        c = input.nextInt();
+        // a nın tüm durumları
+        if((a > b) && (a > c)){
+            if(b > c){
+                System.out.println("a > b > c");
+            }else{
+                if(b < c){
+                    System.out.println("a > c > b");
+                }else{
+                    if(b == c){
+                        System.out.println("a > c = b");
+                    }else{
+                        System.out.println(" a = b = c");
+                    }
+                }
+            }//b nin tüm durumları
+        }else if((b > a) && (b > c)) {
+            if(a>c){
+                System.out.println("b > a > c");
+            }else{
+                if(a < c){
+                    System.out.println("b > c > a");
+                }else{
+                    if(a == c){
+                        System.out.println("b > a = c");
+                    }else{
+                        System.out.println(" a = b = c");
+                    }
+                }
+            }//c nin tüm durumları
+        }else if((c > a) && (c>b)){
+            if(a>b){
+                System.out.println("c > a > b");
+            }else{
+                if(a < b){
+                    System.out.println("c > b > a");
+                }else{
+                    if(a == b){
+                        System.out.println("c > a = b");
+                    }else{
+                        System.out.println(" a = b = c");
+                    }
+                }
+            }
+        }else if((a == b)){
+            if(a > c){
+                System.out.println("a = b > c");
+            }else{
+                if (a == c){
+                    System.out.println("a = b = c");
+                }else{
+                    System.out.println("c > a = b");
+                }
+            }
+        }else if((a == c)){
+            if(a > b){
+                System.out.println("a = c > b");
+            }else{
+                if (a == b){
+                    System.out.println("a = b = c");
+                }else{
+                    System.out.println("b > a = c");
+                }
+            }
+        }else if((b == c)){
+            if(b > a){
+                System.out.println("b = c > a");
+            }else{
+                if (a == b){
+                    System.out.println("a = b = c");
+                }else{
+                    System.out.println("a > b = c");
+                }
+            }
+        }else{
+            System.out.println(" a = b = c");
+        }
+    }
+}
