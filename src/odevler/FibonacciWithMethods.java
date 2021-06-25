bf547ced316936fa74c4cce8430c120d1a7acab4
Murat Batak <muratbatak052005@gmail.com>
2021-06-25 21:53:37 +0300

    Fibonacci methodlarla

---
+++ src/odevler/FibonacciWithMethods.java
@@ -0,0 +1,14 @@
+package odevler;
+
+public class FibonacciWithMethods {
+    static int fibo(int n) {
+        if (n == 1 || n == 2) {
+            return 1;
+        }
+        return fibo(n - 1) + fibo(n - 2);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(fibo(1));
+    }
+}
